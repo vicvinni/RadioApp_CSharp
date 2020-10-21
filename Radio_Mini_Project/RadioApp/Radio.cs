f07fea6b6a5a55bc0d53868fed51288ee7cf7710
vicvinni <63107228+vicvinni@users.noreply.github.com>
2020-10-21 11:59:57 +0100

    Initial Commit- Radio APP

---
+++ Radio_Mini_Project/RadioApp/Radio.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace RadioApp
+{
+    public class Radio
+    {
+        private int _channel = 1; 
+        private bool _on = false;
+
+        //getter and setter for the properties
+        public int Channel {
+            get { return _channel;  }
+            set {
+                if (value <= 4 && value >= 1 && _on)
+                {
+                    _channel = value; 
+                }
+            }
+        }
+
+        //empty/default Constructor
+        public Radio() { }
+
+        public Radio(bool ON, int Channel)
+        {
+            _on = ON;
+            _channel = Channel; 
+        }
+
+        //methods
+        public string Play()
+        {
+            if (!_on)
+            {
+                return $"Radio is off";
+            }
+            else {
+                return $"Playing channel {Channel}";
+            }
+        }
+
+        public void TurnOn()
+        {
+            _on = true; 
+        }
+
+        public void TurnOff()
+        {
+            _on = false;
+        }
+
+    }
+    // implement a class Radio that corresponds to the Class diagram 
+    //   and specification in the Radio_Mini_Project document
+}
---
+++ Radio_Mini_Project/radioGUI/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RadioApp; 
+
+namespace radioGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Radio radio = new Radio(); 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioON_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnRadioON.IsEnabled)
+            {
+                radio.TurnOn();
+                
+                txtRadioDisplay.Text = radio.Play();
+                btnChnl1.IsEnabled = true;
+                btnChnl2.IsEnabled = true;
+                btnChnl3.IsEnabled = true;
+                btnChnl4.IsEnabled = true;
+                sldChnlControl.IsEnabled = true; 
+            }
+           
+        }
+        private void RadioOFF_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnRadioOFF.IsEnabled)
+            {
+                radio.TurnOff();
+                txtRadioDisplay.Text = radio.Play();
+                btnChnl1.IsEnabled = false;
+                btnChnl2.IsEnabled = false;
+                btnChnl3.IsEnabled = false;
+                btnChnl4.IsEnabled = false;
+                sldChnlControl.IsEnabled = false; 
+            }
+        }
+
+        private void btnChnl1_Click(object sender, RoutedEventArgs e)
+        {
+            radio.Channel = 1;
+            txtRadioDisplay.Text = radio.Play();
+           
+
+        }
+        private void btnChnl2_Click(object sender, RoutedEventArgs e)
+        {
+            radio.Channel = 2;
+            txtRadioDisplay.Text = radio.Play();
+        }
+
+        private void btnChnl3_Click(object sender, RoutedEventArgs e)
+        {
+            radio.Channel = 3;
+            txtRadioDisplay.Text = radio.Play();
+        }
+
+        private void btnChnl4_Click(object sender, RoutedEventArgs e)
+        {
+            radio.Channel = 4;
+            txtRadioDisplay.Text = radio.Play();
+        }
+
+        private void sldChnlControl_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double value = Math.Round(sldChnlControl.Value, 0.0); 
+            
+            if(value == 1)
+            {
+                radio.Channel = 1; 
+                txtRadioDisplay.Text = radio.Play();
+            }
+            else if(value == 2)
+            {
+                radio.Channel = 2;
+                txtRadioDisplay.Text = radio.Play();
+            }
+            else if (value == 3)
+            {
+                radio.Channel = 3;
+                txtRadioDisplay.Text = radio.Play();
+            }
+            else if (value == 4)
+            {
+                radio.Channel = 4;
+                txtRadioDisplay.Text = radio.Play();
+            }
+        }
+    }
+}
